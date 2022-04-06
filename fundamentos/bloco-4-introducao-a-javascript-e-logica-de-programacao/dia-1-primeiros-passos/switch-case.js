7b5a8743555a3869bdd773a05ce042296fcf59f2
J. Gouveia <gouvik.dev@gmail.com>
2022-04-06 18:31:45 -0300

    push inicial

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/switch-case.js
@@ -0,0 +1,34 @@
+// let traficLight = "verde";
+
+// switch (traficLight) {
+// 	case "vermelho":
+// 		console.log("Pare!");
+// 		break;
+
+// 	case "amarelo":
+// 		console.log("Atenção!");
+// 		break;
+
+// 	case "verde":
+// 		console.log("Siga!");
+// 		break;
+
+// 	default:
+// 		console.log("Tu é daltônico?");
+// }
+
+let statusPS = "reprovada";
+switch (statusPS) {
+	case "aprovada":
+		console.log("Você está aprovada(o)!");
+		break;
+	case "lista":
+		console.log("Você está na lista de espera.");
+		break;
+	case "reprovada":
+		console.log("Não foi dessa vez. Tenta novamente.");
+		break;
+
+	default:
+		console.log("Status indefinido. Entre em contato com a CES.");
+}
