b91b5ef278f86e519cb7753c31cc6dcd021cb5f8
J. Gouveia <gouvik.dev@gmail.com>
2022-04-27 18:29:14 -0300

    Adicionado o bootstrap e refatorado para preservar a responsividade

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,1 @@
+/* Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.*/
