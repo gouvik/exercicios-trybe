75a4003ea6dcc560461bf11fca60d05270cdc71d
J. Gouveia <gouvik.dev@gmail.com>
2022-05-02 15:26:29 -0300

    kaka

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,1 +1,4 @@
 /* Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.*/
+const DATA_PICKER = document
+	.getElementById("begin-date-input")
+	.DatePickerX.init();
