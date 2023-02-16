df6d7171792a0a1adba27dcb64a9e01111b705e5
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-15 23:31:04 -0300

    fix: Um teste

--- CS/dia_3/pokemon.py
+++ CS/dia_3/pokemon.py
@@ -3,7 +3,6 @@ import json
 
 def retrieve_pokemons_by_type(type, reader):
     # lê o conteudo de reader e o transforma de json
-    # para dicionário
     pokemons = json.load(reader)["results"]
     # filtra somente os pokemons do tipo escolhido
     pokemons_by_type = [
